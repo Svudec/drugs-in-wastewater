2fdfd293e87e939a4328e34fe4ac50a293511c59
Karlo Sudec <karlo.sudec@gmail.com>
2022-12-18 22:13:03 +0100

    add avg metabolite by country

--- backend/endpoints/metaboliteEndpoints.js
+++ backend/endpoints/metaboliteEndpoints.js
@@ -6,6 +6,32 @@ router.route('/').get(getAll('metabolite'))
 
 router.route('/:id').get(getById('metabolite'))
 
+const getAvgValues = (metaboliteId, year) => ({
+    text: `select c2.*, avg(value) value
+    from measurement m
+        join metabolite m2
+            on m2.id = m.metabolite_id and m2.id = $1 ${year ? 'and year = $2' : ''}
+        join location l
+            on m.location_id = l.id
+        join city c
+            on l.city_id = c.id
+        join country c2
+            on c.country_id = c2.country_code
+        group by c2.country_code, c2.name, c2.long_name
+        order by value desc`, 
+        values: [metaboliteId].concat(year ? [year] : [])
+})
+
+router.route('/avg-by-country/:id').get(async (req, res) => {
+    const queryRes = await executeQuery(getAvgValues(req.params.id))
+    sendResponseGet(queryRes, res)
+})
+
+router.route('/avg-by-country/:id/:year').get(async (req, res) => {
+    const queryRes = await executeQuery(getAvgValues(req.params.id, req.params.year))
+    sendResponseGet(queryRes, res)
+})
+
 module.exports = {
     default: router
 }
