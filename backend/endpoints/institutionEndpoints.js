960bdfe33c5184bcd9a89f36afa0eba82b079f89
Karlo Sudec <karlo.sudec@gmail.com>
2023-01-04 00:27:04 +0100

    add country to institution

--- backend/endpoints/institutionEndpoints.js
+++ backend/endpoints/institutionEndpoints.js
@@ -1,8 +1,19 @@
 const { Router } = require('express')
-const { getAll, getById } = require('../queries')
+const { getById, executeQuery, sendResponseGet } = require('../queries')
 const router = Router()
 
-router.route(`/api/v1/institution`).get(getAll('institution'))
+const getWithCountriesQuery = `
+select distinct i.*, co.country_code country_code, co.long_name country
+from location l
+    join institution i on l.institution_id = i.id
+    join city c on l.city_id = c.id
+    join country co on c.country_id = co.country_code
+order by country_code`
+
+router.route(`/api/v1/institution`).get(async (req, res) => {
+    const queryRes = await executeQuery(getWithCountriesQuery)
+    sendResponseGet(queryRes, res)
+})
 
 router.route(`/api/v1/institution/:id`).get(getById('institution'))
 
