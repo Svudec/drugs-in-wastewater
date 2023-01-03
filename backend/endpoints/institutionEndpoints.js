c963ea45a448adef5b73d30fa219e7172f074d54
Karlo Sudec <karlo.sudec@gmail.com>
2023-01-04 00:47:16 +0100

    finished jsonLd

--- backend/endpoints/institutionEndpoints.js
+++ backend/endpoints/institutionEndpoints.js
@@ -2,6 +2,15 @@ const { Router } = require('express')
 const { getById, executeQuery, sendResponseGet } = require('../queries')
 const router = Router()
 
+const jsonLd = {
+    "@context": {
+        "@vocab": "http://schema.org/ResearchOrganization",
+        "id": "identifier",
+        "name": "legalName",
+        "website": "url"
+    }
+}
+
 const getWithCountriesQuery = `
 select distinct i.*, co.country_code country_code, co.long_name country
 from location l
@@ -11,7 +20,7 @@ from location l
 order by country_code`
 
 router.route(`/api/v1/institution`).get(async (req, res) => {
-    const queryRes = await executeQuery(getWithCountriesQuery)
+    const queryRes = await executeQuery(getWithCountriesQuery, false, jsonLd)
     sendResponseGet(queryRes, res)
 })
 
